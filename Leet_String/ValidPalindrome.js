bfdd48c1d83cfc225a1d69782c4a23f16f1dc5f5
Mr-React <ambureomkar@gmail.com>
2023-10-26 11:33:05 +0530

    Valid Palindrome

---
+++ Leet_String/ValidPalindrome.js
@@ -0,0 +1,54 @@
+/**
+ * Valid Palindrome
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ */
+
+var isPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    if (!isAlphaNumeric(s[left])) left++;
+    else if (!isAlphaNumeric(s[right])) right--;
+    else if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+    else {
+      left++;
+      right--;
+    }
+  }
+  return true;
+};
+
+function isAlphaNumeric(char) {
+  let code = char.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) ||
+    (code >= 65 && code <= 90) ||
+    (code >= 97 && code <= 122)
+  );
+}
+
+console.log("A man, a plan, a canal: Panama");
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log("\nrace a car");
+console.log(isPalindrome("race a car"));
